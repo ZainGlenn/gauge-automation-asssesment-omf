fbc3438a0d9edb3cd44afebb094002963b961568
Zain Glenn <zain.glenn@infocarehealth.com>
2021-03-03 21:38:17 +0200

    no message

---
+++ src/main/java/org/gauge/omf/components/Label.java
@@ -0,0 +1,9 @@
+package org.gauge.omf.components;
+
+import org.openqa.selenium.By;
+
+public class Label extends Element<Label> {
+    public Label(By locator) {
+        super(locator);
+    }
+}
---
+++ src/main/java/org/gauge/omf/exception/OMFRuntimeException.java
@@ -0,0 +1,7 @@
+package org.gauge.omf.exception;
+
+public class OMFRuntimeException extends RuntimeException {
+    public OMFRuntimeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
